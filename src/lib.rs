a7be4050b2cbfc53d2e4332d232751caab3fa78d
Jeffrey Emakpor <jeffreyefemena4@gmail.com>
2024-03-21 14:46:20 +0100

    fix: string length and add test

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+#![allow(
+dead_code,
+unused_imports
+)]
+
 mod constants;
 mod types;
 
@@ -9,28 +14,35 @@ mod generate_api_keys {
   pub fn random_string(options: &StringGenerationOptions) -> String {
 
     let default_pool = get_default_character();
-
     let min = options.min.unwrap_or(DEFAULT_MIN_LENGTH);
-    let max = options.min.unwrap_or(DEFAULT_MAX_LENGTH);
+    let max = options.max.unwrap_or(DEFAULT_MAX_LENGTH);
+
     let pool= match &options.pool {
         Some(s) => s,
         None => &default_pool
     };
-    let prefix = &options.prefix.clone().unwrap();
-    let mut g = generate_random_string(pool,  min,  max);
+
+    let prefix = options.prefix.clone().unwrap_or("".to_string());
+    let length = match options.length {
+        Some(l) => l,
+        None => {
+          rand::thread_rng().gen_range(min ..=max)
+        }
+    };
+
+    let mut g = generate_random_string(pool,  length);
 
     add_prefix( &mut g, &prefix)
   }
   
-  fn generate_random_string(pool: &String, min_length: u8, max_length: u8) -> String {
-      let mut rng = rand::thread_rng();
-      let length = rng.gen_range(min_length ..max_length + 1);
-      (0..length)
-          .map(|_| {
-              let idx = rng.gen_range(0..pool.len());
-              pool.chars().nth(idx).unwrap()
-          })
-          .collect()
+  fn generate_random_string(pool: &String, length: u8) -> String {
+    let mut rng = rand::thread_rng();
+    (0..length)
+        .map(|_| {
+            let idx = rng.gen_range(0..pool.len());
+            pool.chars().nth(idx).unwrap()
+        })
+        .collect()
   }
 
   fn add_prefix(start_string: &mut String, concat: &String) -> String {
@@ -45,17 +57,46 @@ mod tests{
   use crate::{generate_api_keys::random_string, types::StringGenerationOptions};
 
   #[test]
-  fn test_random_string(){
+  fn test_random_string_with_prefix(){
     let options = StringGenerationOptions {
       length:None,
-      max: None,
       min: None,
+      max: None,
       pool: None,
       prefix: Some(String::from("PREFIX-")),
     };
 
     let result = random_string(&options);
+    assert!(&result.starts_with("PREFIX"));
+  }
 
-    assert!(&result.starts_with("PREFIX"))
+  #[test]
+  fn test_random_string_with_min_max() {
+    let options = StringGenerationOptions {
+      length:None,
+      min: Some(5),
+      max: Some(8),
+      pool: None,
+      prefix: None,
+    };
+
+    let result = random_string(&options);
+    let result_length = result.len();
+    assert!(result_length >= 5 || result_length >= 8);
+  }
+
+  #[test]
+  fn test_random_string_with_length() {
+    let options = StringGenerationOptions {
+      length:Some(10),
+      min: None,
+      max: None,
+      pool: None,
+      prefix: None,
+    };
+
+    let result = random_string(&options);
+
+    assert!(result.len() >= 10);
   }
 }
